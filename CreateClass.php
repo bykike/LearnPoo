3a055e0665aa3371a2ba499981847f934cdaf598
Kike Algarra <kike@areavisual.net>
2021-10-27 12:34:42 +0200

    La clase y el objeto
    La clase es como una plantilla que define características y funciones. El objeto agrupa los datos de la clase y permite utilizarlos desde una unidad.

--- CreateClass.php
+++ CreateClass.php
@@ -2,6 +2,15 @@
 
 class Cohete {
     public $color;
+    public $potencia;
+    public $marca;
 }
+
+$miCohete = new Cohete();
+$miCohete->color = 'rojo';
+$miCohete->potencia = 120;
+$miCohete->marca = 'audi';
+
+echo 'Color del Cohete: ' . $miCohete->color;
 
 ?>
